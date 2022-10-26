b833315d34a1ad68f3861b312428313e8bc831be
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-26 16:45:03 +0530

    Connecting to the database and installing packages

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+// Creating the connection with the database
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() =>
+		console.log("Successfully Connected to the MongoDB Database...!"),
+	)
+	.catch((err) => {
+		console.log(err);
+	});
+
+// Middleware
+app.use(express.json());
+
+// Creating the port connection of the Backend Server
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Listening on port " + process.env.PORT || 5000, "\b...!");
+});
