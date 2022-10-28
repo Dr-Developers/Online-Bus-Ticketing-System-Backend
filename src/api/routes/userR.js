79642aa89f563227f9d5592eb99fe592f3965c0d
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-28 18:04:20 +0530

    implementing retrieve, update, delete, retrieveOne user routes

--- src/api/routes/userR.js
+++ src/api/routes/userR.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
-const { userAdd } = require("../controller/userC");
+const {
+	userAdd,
+	deleteUser,
+	getOneUser,
+	retrieveAllUsers,
+	updateUser,
+} = require("../controller/userC");
 
 // defining user routes
 router.post("/add", userAdd);
+router.delete("/delete/:id", deleteUser);
+router.get("/", retrieveAllUsers);
+router.get("/:id", getOneUser);
+router.put("/update/:id", updateUser);
 
 module.exports = router;
