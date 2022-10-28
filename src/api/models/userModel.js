63b02e6edba40f1710bf3411a76451292c808551
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-28 11:33:03 +0530

    Adding new attributes to User Model

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
         userID: { type: String, required: true },
         name: { type: String, required: true },
         email: { type: String, required: true },
+        nicType: { type: String, required: true },
+        number: { type: String, required: true },
         phoneNumber: { type: String, required: true },
         username: { type: String, required: true },
         password: { type: String, required: true },
