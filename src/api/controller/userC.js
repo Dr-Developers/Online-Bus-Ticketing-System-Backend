14c44946f798bbb7979ba18eb892f52405bde9af
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-28 18:03:09 +0530

    Implementing retrieve, delete, update and retrieveOne functions

--- src/api/controller/userC.js
+++ src/api/controller/userC.js
@@ -1,37 +1,55 @@
-const bcryptjs = require("bcryptjs");
+// noinspection UnreachableCodeJS
+
+const CryptoJS = require("crypto-js");
 const User = require("../models/userM");
 const { user_validation } = require("../validation/userValidation");
 
+let userI = 0;
+
 // creating a new user in the DB
 const userAdd = async (req, res) => {
-	let id = 1;
 	// validating inputs whether they are empty
 	const { error } = user_validation(req.body);
 
 	// if error occurred display the error msg
 	if (error) {
-		res.send({ message: error["details"][0]["message"] });
+		return res.send({ message: error["details"][0]["message"] });
 	} else {
 		// checking whether the user already exist or not
-		const UserExist = await User.findOne(
-			{ email: req.body.email },
-			{ number: req.body.number },
-		);
+		const EmailExist = await User.findOne({ email: req.body.email });
 
-		if (UserExist) {
+		const NICExist = await User.findOne({ number: req.body.number });
+
+		const UsernameExist = await User.findOne({
+			username: req.body.username,
+		});
+
+		// taking the last document of the mongo User collection
+		const last = await User.find().sort({ _id: -1 });
+
+		// checking whether the array is not empty
+		if (!(last.length === 0)) {
+			// console.log("Last: ", parseInt(last[0].userID.split("0")[1]));
+			userI = parseInt(last[0].userID.split("0")[1]);
+		}
+
+		if (EmailExist || NICExist) {
 			return res
 				.status(400)
 				.send({ message: "User Already Exist!" });
+		} else if (UsernameExist) {
+			return res
+				.status(400)
+				.send({ message: "Username Already Exist!" });
 		} else {
 			// encrypting the password
-			const salt = await bcryptjs.getSalt(5);
-			const encryptedPassword = await bcrypt.hash(
+			const encryptedPassword = CryptoJS.AES.encrypt(
 				req.body.password,
-				salt,
-			);
+				process.env.PASS_SECRET,
+			).toString();
 
 			const user = new User({
-				userID: id,
+				userID: "U0" + (userI + 1),
 				name: req.body.name,
 				email: req.body.email,
 				nicType: req.body.nicType,
@@ -42,19 +60,110 @@ const userAdd = async (req, res) => {
 				type: req.body.type,
 			});
 
-			// testing
-			console.log("user: ", user);
-
 			try {
 				const savedUser = user.save();
+				console.log("========================");
 				console.log("User Saved Successfully!");
-				res.send(savedUser);
-				id = id + 1;
+				console.log("========================\n");
+				return res.status(200).send({ savedUser: user });
 			} catch (error) {
-				res.send({ message: error });
+				return res.status(400).send({ message: error });
 			}
 		}
 	}
 };
 
-module.exports = { userAdd };
+const retrieveAllUsers = async (req, res) => {
+	try {
+		const users = await User.find();
+		return res.status(200).send({ users });
+	} catch (error) {
+		return res.status(400).send({ message: error });
+	}
+};
+
+const getOneUser = async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id });
+
+		if (!user) {
+			return res.status(404).send({ message: "User Not Found!" });
+		} else {
+			return res.status(200).send({ user });
+		}
+	} catch (error) {
+		return res.status(400).send({ message: error });
+	}
+};
+
+const deleteUser = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			return res.status(404).send({ message: "User Not Found!" });
+		} else {
+			const deletedUser = await User.findByIdAndDelete(
+				req.params.id,
+			);
+
+			console.log("==========================");
+			console.log("User Deleted Successfully!");
+			console.log("==========================\n");
+
+			return res.status(200).send({ deletedUser });
+		}
+	} catch (error) {
+		return res.status(400).send({ message: error });
+	}
+};
+
+const updateUser = async (req, res) => {
+	const paramsID = req.params.id;
+	try {
+		const user = await User.findById(paramsID);
+		if (!user) {
+			return res.status(404).send({ message: "User Not Found!" });
+		} else {
+			const {
+				name,
+				email,
+				nicType,
+				number,
+				phoneNumber,
+				username,
+				password,
+				type,
+			} = req.body;
+
+			const updatedUser = await User.findByIdAndUpdate(paramsID, {
+				name,
+				email,
+				nicType,
+				number,
+				phoneNumber,
+				username,
+				password,
+				type,
+			});
+
+			console.log("==========================");
+			console.log("User Updated Successfully!");
+			console.log("==========================\n");
+
+			return res.status(200).send({ updatedUser });
+		}
+	} catch (error) {
+		return res.status(400).send({ message: error });
+	}
+};
+
+/*
+ * exporting functions
+ */
+module.exports = {
+	userAdd,
+	retrieveAllUsers,
+	getOneUser,
+	deleteUser,
+	updateUser,
+};
