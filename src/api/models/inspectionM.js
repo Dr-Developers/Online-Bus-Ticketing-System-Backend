6da0c23c086191dfa9f3d0877db34e9911478565
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-28 14:01:44 +0530

    changing file name

---
+++ src/api/models/inspectionM.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const inspectionSchema = new mongoose.Schema(
+	{
+		routeId: { type: String, required: true },
+		tie: { type: String, required: true },
+		date: { type: String, required: true },
+		inspectorName: { type: String, required: true },
+	},
+
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model("Inspection", inspectionSchema);
