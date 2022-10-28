56669bf9c1314c23e4d76e217d01f3650f5ca7f8
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-10-28 18:03:42 +0530

    make payment routes

---
+++ src/api/routes/paymentR.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const {
+    addPayment,
+    getPayment,
+    updatePayment,
+    deletePayment,
+    getonePayment,
+} = require("../controller/paymentController");
+const Event = require("../models/paymentM");
+
+//define user routes
+router.post("/add", addPayment);
+router.get("/all", getPayment);
+router.put("/update/:id", updatePayment);
+router.delete("/delete/:id", deletePayment);
+router.get("/:id", getonePayment);
+
+module.exports = router;
