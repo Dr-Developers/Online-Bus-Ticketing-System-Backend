fd01ded56dc72b4e0a29f884e8217ec61950d700
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-10-31 04:11:48 +0530

    Implementing inspection model

--- src/api/models/inspectionM.js
+++ src/api/models/inspectionM.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
+
 const inspectionSchema = new mongoose.Schema(
 	{
 		routeId: { type: String, required: true },
-		tie: { type: String, required: true },
+		type: { type: String, required: true },
 		date: { type: String, required: true },
 		inspectorName: { type: String, required: true },
 	},
