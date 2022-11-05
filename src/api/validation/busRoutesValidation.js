1c4251b6330a4739b78be2aed038668a4e34e40f
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-06 01:03:56 +0530

    Implement bus route validation

--- src/api/validation/busRoutesValidation.js
+++ src/api/validation/busRoutesValidation.js
@@ -9,7 +9,7 @@ const busRoutes_validation = (data) => {
         date: Joi.string().required(),
         startLocation: Joi.string().required(),
         EndLocation: Joi.string().required(),
-        vType: Joi.string().required(),
+        
     });
 
     return schema_validation.validate(data);
