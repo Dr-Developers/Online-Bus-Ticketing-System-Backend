c599ea503476e941c5fccdd6da26061a74754c9b
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-06 01:03:23 +0530

    Implement bus route model

--- src/api/models/busRoutesM.js
+++ src/api/models/busRoutesM.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const busRoutesSchema = new mongoose.Schema({
-        timetableID: { type: String, required: true },
+        timetableID: { type: String},
         vehicleNo: { type: String, required: true },
         routeId: { type: String, required: true },
         time: { type: String, required: true },
         date: { type: String, required: true },
         startLocation: { type: String, required: true },
         EndLocation: { type: String, required: true },
-        vType: { type: String, required: true },
+        
     },
 
     { timestamps: true },
