b5c5cde3202e05178b91f7fe0745d86f8c893566
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-06 01:03:37 +0530

    Implement payment model

---
+++ src/api/models/paymentM.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+        paymentID: { type: String},
+        amount: { type: Number, required: true },
+        email: { type: String, required: true },
+        holderName: { type: String, required: true },
+        cardNumber: { type: String, required: true },
+        cvv: { type: String, required: true },
+        expDate: { type: String, required: true },
+    },
+
+    { timestamps: true },
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);
